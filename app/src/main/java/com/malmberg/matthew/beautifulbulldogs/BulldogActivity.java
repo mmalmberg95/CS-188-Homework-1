acdc9a342d4e85f5e7c9478d27e50c94f906b7b7
mmalmberg95 <30853126+mmalmberg95@users.noreply.github.com>
2017-09-25 15:54:30 -0500

    round one

--- app/src/main/java/com/malmberg/matthew/beautifulbulldogs/BulldogActivity.java
+++ app/src/main/java/com/malmberg/matthew/beautifulbulldogs/BulldogActivity.java
@@ -11,7 +11,7 @@ public class BulldogActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_bulldog2);
+        setContentView(R.layout.activity_bulldog);
 
         textView = (TextView) findViewById(R.id.textView);
 
